b4bcc70ffc99b7c7bd026632700a9f741f08af3d
Simon <simonhgillespie@gmail.com>
2018-10-11 19:07:35 +0100

    Upload of question 1

--- src/q1/assignment.c
+++ src/q1/assignment.c
@@ -1,6 +1,25 @@
 #include <stdio.h>
 
 int main(int arg, char* argc[]){
-  printf("Hello assignment1.\n");
+	
+	int x;
+	int *y;
+	y = &x;
+
+	printf("The size of an int is: %d\n", sizeof(x));
+	printf("The size of an int pointer is: %d\n", sizeof(y));
+
+	long a;
+	printf("The size of a long is: %d\n", sizeof(a));
+
+	double *c;
+	printf("The size of a double pointer is: %d\n", sizeof(c));
+
+	char **test;
+	printf("The size of a char double pointer is: %d\n", sizeof(test));
+
+	//The size of all these variables and pointers is 4 bytes. 
+  
+  
   return 0;
 }
